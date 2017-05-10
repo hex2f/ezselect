5054fb3f07fe3923ee44d0772ada44f65bfe7f80
hampus <haslun16@edu.vilhelmina.se>
2017-05-10 19:24:05 +0200

    Removed EZElement object.

--- index.js
+++ index.js
@@ -6,17 +6,11 @@ try {
   throw 'Could not find document. Please use ez(el, doc) to use the correct document.';
 }
 
-function EZElement(elmname) {
-  this.el = doc_node.querySelector(elmname);
-  return this.el;
-}
-
 module.exports = function(elm, doc) {
   if(doc_node == undefined) throw 'Document node is not set. Please use ez(elm, doc) to use the correct document.';
   if(doc != undefined) doc_node = doc;
   try {
-    console.log(elm)
-    return new EZElement(elm);
+    return doc_node.querySelector(elmname);
   } catch (e) {
     throw `Oops. Something went wrong.  Error: ${e}`;
   }
